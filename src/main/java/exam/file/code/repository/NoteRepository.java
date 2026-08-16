e3a56ae13aeb3ce80b69fd7f2f26e897cc4db8f8
harena <tsutoru24@gmail.com>
2026-08-16 19:31:05 +0300

    feat: add repositories for Note, NoteHistorique and CourseTeamAssignment

---
+++ src/main/java/exam/file/code/repository/NoteRepository.java
@@ -0,0 +1,13 @@
+package exam.file.code.repository;
+
+import exam.file.code.Entity.Note;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoteRepository extends JpaRepository<Note, UUID> {
+
+  List<Note> findByStudentId(String studentId);
+
+  List<Note> findByExamenCoursId(UUID coursId);
+}
