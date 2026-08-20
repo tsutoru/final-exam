d3a04ed5ff93393ff7369502bb1fa0d0a7cffe97
harena <tsutoru24@gmail.com>
2026-08-20 15:51:45 +0300

    style : change to the request format

--- src/main/java/exam/file/code/dto/ExamenCreateDto.java
+++ src/main/java/exam/file/code/dto/ExamenCreateDto.java
@@ -4,4 +4,5 @@ import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.UUID;
 
-public record ExamenCreateDto(String name, Instant dateTime, BigDecimal coefficient, UUID coursId) {}
+public record ExamenCreateDto(
+    String name, Instant dateTime, BigDecimal coefficient, UUID coursId) {}
--- src/main/java/exam/file/code/dto/ExamenDto.java
+++ src/main/java/exam/file/code/dto/ExamenDto.java
@@ -4,4 +4,5 @@ import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.UUID;
 
-public record ExamenDto(UUID id, String name, Instant dateTime, BigDecimal coefficient, UUID coursId) {}
+public record ExamenDto(
+    UUID id, String name, Instant dateTime, BigDecimal coefficient, UUID coursId) {}
