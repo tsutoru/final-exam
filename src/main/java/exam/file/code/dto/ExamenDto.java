8b56815011a596a7f3cab32f58d3749dd7afdeaa
harena <tsutoru24@gmail.com>
2026-08-20 15:35:53 +0300

    feat: add Examen DTOs

---
+++ src/main/java/exam/file/code/dto/ExamenDto.java
@@ -0,0 +1,7 @@
+package exam.file.code.dto;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+public record ExamenDto(UUID id, String name, Instant dateTime, BigDecimal coefficient, UUID coursId) {}
